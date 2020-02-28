da25e4b327a4115dc48e075c1ee6d30a5b48a4b9
Logan Sorensen <loganmsorensen@gmail.com>
2020-02-27 18:05:58 -0800

    added tests for the endpoints

--- database/connection.js
+++ database/connection.js
@@ -5,3 +5,9 @@ const knexfile = require("../knexfile");
 const env = process.env.DB_ENV || "development";
 
 module.exports = knex(knexfile[env]);
+
+
+// const knex = require("knex");
+// const config = require("../knexfile");
+// const env = process.env.DB_ENV || "development";
+// module.exports = knex(config[env]);
